94b6d2df3ec8f1ac41707e649874339bd8f20ed0
safiul <safiul.alam@nordcloud.fi>
2023-10-20 19:17:12 +0300

    f

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const sequelize = require("./database");
+const User = require("./User");
 
-sequelize.sync().then(() => console.log("db is running"));
+sequelize.sync({ force: true }).then(() => console.log("db is running"));
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 
+app.get("/users", async (req, res) => {
+  const users = await User.findAll();
+  res.send(users);
+});
+
+app.post("/users", async (req, res) => {
+  await User.create(req.body);
+  res.send("User created successfully");
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
